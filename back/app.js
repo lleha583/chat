58b35be93601efabf6a24a2f7dbac7df77af8a38
lleha583 <vip.alekseyklepcha@mail.ru>
2025-01-01 16:08:58 +0300

    wqrwe

--- back/app.js
+++ back/app.js
@@ -10,7 +10,7 @@ app.use(cors())
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-        res.send('Hello')
+        res.send('Heerrtyllo')
 })
 
 app.get('/user', (req, res) => {
