4717cfd735a12513b84b4b1e6d7e931d70031c82
lleha583 <lleha583@gmail.com>
2025-01-01 15:48:44 +0300

    test

--- back/app.js
+++ back/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const cors = require('cors')
 require('dotenv').config
 
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
         res.send('Hello')
+})
+
+app.get('/user', (req, res) => {
+        res.json({status: 200, res: 'hello dayn'})
 })
 
 app.listen(PORT, () => console.log(`http://localhost:3000`))
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
+
+  useEffect(() => {
+    const setUser = async () => {
+      const res = await fetch('http://localhost:3000/user', { method: "GET" })
+      const response = await res.json()
+      await console.log(response);
+    } 
+    setUser()
+  }, [])
 
   return (
     <>
